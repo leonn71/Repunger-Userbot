364b7c53a370472deb7acaee36e3b69afca3d772
mutyya <bimasakti2d@gmail.com>
2025-04-26 00:37:35 +0700

    Update start.py

--- AyiinXd/ayiin/start.py
+++ AyiinXd/ayiin/start.py
@@ -18,10 +18,10 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/b17248930f58bbdaf2e23.jpg",
-                caption="𝗜𝗫𝗔𝗟𝗟-Userbot.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
-                buttons=[(Button.url("Store", "https://t.me/jasebxall")),
-                         (Button.url("Support", "https://t.me/ixallsupport"))]
+                "https://telegra.ph/file/7881245cdf92e3b0c0d33-8e3772b60440d5e997.jpg",
+                caption="𝗠𝘂𝘁𝘆𝗮-𝗨𝘀𝗲𝗿𝗯𝗼𝘁.\n     **status : Active\n     ketik `.ping` untuk cek bot!**",
+                buttons=[(Button.url("Store", "https://t.me/jasebimuut")),
+                         (Button.url("Support", "https://t.me/imutsupport"))]
             )
     except Exception as e:
         LOGS.error(e)
